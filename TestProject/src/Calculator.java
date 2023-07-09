6be495d5bc4f7f04007efb25e04a008024a0c073
POOJA <POOJA@DESKTOP-05VNFG8>
2023-07-09 19:19:39 +0530

    Second commit

---
+++ TestProject/src/Calculator.java
@@ -0,0 +1,17 @@
+
+public class Calculator {
+
+	int n1=5;
+	int n2=2;
+	public void add()
+	{
+		System.out.println("sum is" + (n1+n2));
+	}
+	public static void main(String[] args) {
+
+		Calculator calculator=new Calculator();
+		calculator.add();
+
+	}
+
+}
